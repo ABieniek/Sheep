56d93d0167989ed737d3b5f81b1a311921903c8a
Alex Bieniek <A.Bieniek789@gmail.com>
2017-07-30 15:04:31 -0500

    penetration vector's direction is now being calculated correctly, but the magnitude of penetration doesn't look accurate

--- Sheep/CollisionUtil.cpp
+++ Sheep/CollisionUtil.cpp
@@ -15,7 +15,7 @@ glm::vec2 penetrationVector(glm::vec2 position1, GLfloat radius1, glm::vec2 posi
 {
 	// for each component, the amount of penetration should be r1 + r2 - (c1 - c2)
 	cout << norm(position2 - position1) << endl;
-	return glm::normalize(position2 - position1) * (sqrt(norm(position2 - position1)) - (radius2 + radius1));
+	return -(glm::normalize(position2 - position1) * (sqrt(norm(position2 - position1)) - (radius2 + radius1)/2));
 
 	// @TODO as of right now, this code doesn't handle the case that, within one frame, the units have concentric hurtboxes
 	// the position would be moved by 2r, so they would indeed be placed such that their borders are contiguous
